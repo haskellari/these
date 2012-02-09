9d1422e12482301f634c18a9a3579945223b1070
Elliott Hird <penguinofthegods@gmail.com>
2012-02-09 16:28:27 +0800

    Add empty, Crosswalk, Bicrosswalk and laws

--- Data/Align.hs
+++ Data/Align.hs
@@ -3,64 +3,83 @@
 --
 --   'These'-based zipping and unzipping of structures with
 --   non-uniform shapes.
-module Data.Align (Align(..), Unalign(..)) where
+module Data.Align (
+                    Align(..)
+                  , Unalign(..)
+                  , Crosswalk(..)
+                  , Bicrosswalk(..)
+                  ) where
 
-import Data.These
+-- TODO: More instances..
+
+import Control.Applicative (ZipList(..), pure, (<$>))
+import Data.Bifoldable (Bifoldable(..))
+import Data.Bifunctor (Bifunctor(..))
+import Data.Foldable (Foldable)
 import Data.Functor.Identity (Identity(..))
-import Control.Applicative (ZipList(..), (<$>))
-import Data.Tree (Tree(..))
-import Data.Sequence (Seq)
-import qualified Data.Sequence as Seq
-import Data.Map (Map)
-import qualified Data.Map as Map
 import Data.IntMap (IntMap)
+import Data.Map (Map)
+import Data.Sequence (Seq)
+import Data.These
 import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
 
 oops :: String -> a
 oops = error . ("Data.Align: internal error: " ++)
 
 -- --------------------------------------------------------------------------
--- | Zipping for structures with non-uniform shapes.
+-- | Functors supporting a zip operation that takes the union of
+--   non-uniform shapes.
 --
---   Complete definition: Either @align@ or @alignWith@.
+--   If your functor is actually a functor from @Kleisli Maybe@ to
+--   @Hask@ (so it supports @maybeMap :: (a -> Maybe b) -> f a -> f
+--   b@), then an @Align@ instance is making your functor lax monoidal
+--   w.r.t. the cartesian monoidal structure on @Kleisli Maybe@,
+--   because @These@ is the cartesian product in that category @(a ->
+--   Maybe (These b c) ~ (a -> Maybe b, a -> Maybe c))@. This insight
+--   is due to rwbarton.
+--
+--   Minimal definition: @empty@ and either @align@ or @alignWith@.
 --
 --   Laws:
 --
---   1. @align@ = @alignWith id@
---
---   2. @alignWith f a b@ = @fmap f (align a b)@
---
---   3. @join align@ = @fmap (join Both)@
+-- @
+-- (\`align` empty) = fmap This
+-- (empty \`align`) = fmap That
+-- join align = fmap (join Both)
+-- align (f \<$> x) (g \<$> y) = bimap f g \<$> align x y
+-- alignWith f a b = f \<$> align a b
+-- @
 class (Functor f) => Align f where
+    empty :: f a
+
     align :: f a -> f b -> f (These a b)
     align = alignWith id
 
     alignWith :: (These a b -> c) -> f a -> f b -> f c
     alignWith f a b = f <$> align a b
 
-instance Align Identity where
-    align (Identity a) (Identity b) = Identity (These a b)
-
 instance Align Maybe where
+    empty = Nothing
     align Nothing Nothing = Nothing
     align (Just a) Nothing = Just (This a)
     align Nothing (Just b) = Just (That b)
     align (Just a) (Just b) = Just (These a b)
 
 instance Align [] where
+    empty = []
     align xs [] = This <$> xs
     align [] ys = That <$> ys
     align (x:xs) (y:ys) = These x y : align xs ys
 
 instance Align ZipList where
+    empty = ZipList []
     align (ZipList xs) (ZipList ys) = ZipList (align xs ys)
-
-instance Align Tree where
-    align (Node a xs) (Node b ys) = Node (These a b) (map merge (align xs ys))
-      where merge = these (fmap This) (fmap That) align
 
 -- could probably be more efficient...
 instance Align Seq where
+    empty = Seq.empty
     align xs ys =
         case Seq.viewl xs of
             Seq.EmptyL   -> That <$> ys
@@ -70,31 +89,43 @@ instance Align Seq where
                     y Seq.:< ys' -> These x y Seq.<| align xs' ys'
 
 instance (Ord k) => Align (Map k) where
+    empty = Map.empty
     align m n = Map.unionWith merge (Map.map This m) (Map.map That n)
       where merge (This a) (That b) = These a b
             merge _ _ = oops "Align Map: merge"
 
 instance Align IntMap where
+    empty = IntMap.empty
     align m n = IntMap.unionWith merge (IntMap.map This m) (IntMap.map That n)
       where merge (This a) (That b) = These a b
             merge _ _ = oops "Align IntMap: merge"
 
 -- --------------------------------------------------------------------------
--- | Unzipping for structures of non-uniform shapes.
+-- | Alignable functors supporting an \"inverse\" to 'align': splitting
+--   a union shape into its component parts.
 --
---   Complete definition: nothing; a default definition is provided,
+--   Minimal definition: nothing; a default definition is provided,
 --   but it may not have the desired definition for all functors. See
 --   the source for more information.
+--
+--   Laws:
+--
+-- @
+-- unalign empty               = (empty,         empty)
+-- unalign (This        \<$> x) = (Just    \<$> x, Nothing \<$  x)
+-- unalign (That        \<$> y) = (Nothing \<$  y, Just    \<$> y)
+-- unalign (join These  \<$> x) = (Just    \<$> x, Just    \<$> x)
+-- unalign ((x \`These`) \<$> y) = (Just x  \<$  y, Just    \<$> y)
+-- unalign ((\`These` y) \<$> x) = (Just    \<$> x, Just y  \<$  x)
+-- @
 class (Align f) => Unalign f where
-    -- This needs laws. Specifically, some notion of not duplicating
-    -- the effects is required, and a way to express its relationship
-    -- with @align@.
+    -- This might need more laws. Specifically, some notion of not
+    -- duplicating the effects would be nice, and a way to express its
+    -- relationship with align.
     unalign :: f (These a b) -> (f (Maybe a), f (Maybe b))
     unalign x = (fmap left x, fmap right x)
       where left  = these Just (const Nothing) (\a _ -> Just a)
             right = these (const Nothing) Just (\_ b -> Just b)
-
-instance Unalign Identity
 
 instance Unalign Maybe
 
@@ -108,4 +139,67 @@ instance Unalign ZipList where
     unalign (ZipList xs) = (ZipList ys, ZipList zs)
       where (ys, zs) = unalign xs
 
--- TODO: More obvious instances...
+-- --------------------------------------------------------------------------
+-- | Foldable functors supporting traversal through an alignable
+--   functor.
+--
+--   Minimal definition: @crosswalk@ or @sequenceL@.
+--
+--   Laws:
+--
+-- @
+-- crosswalk (const empty) = const empty
+-- crosswalk f = sequenceL . fmap f
+-- @
+class (Functor t, Foldable t) => Crosswalk t where
+    crosswalk :: (Align f) => (a -> f b) -> t a -> f (t b)
+    crosswalk f = sequenceL . fmap f
+
+    sequenceL :: (Align f) => t (f a) -> f (t a)
+    sequenceL = crosswalk id
+
+instance Crosswalk Identity where
+    crosswalk f (Identity a) = fmap Identity (f a)
+
+instance Crosswalk Maybe where
+    crosswalk _ Nothing = empty
+    crosswalk f (Just a) = Just <$> f a
+
+instance Crosswalk [] where
+    crosswalk _ [] = empty
+    crosswalk f (x:xs) = alignWith cons (f x) (crosswalk f xs)
+      where cons = these pure id (:)
+
+instance Crosswalk (These a) where
+    crosswalk _ (This _) = empty
+    crosswalk f (That x) = That <$> f x
+    crosswalk f (These a x) = These a <$> f x
+
+-- --------------------------------------------------------------------------
+-- | Bifoldable bifunctors supporting traversal through an alignable
+--   functor.
+--
+--   Minimal definition: @bicrosswalk@ or @bisequenceL@.
+--
+--   Laws:
+--
+-- @
+-- bicrosswalk (const empty) (const empty) = const empty
+-- bicrosswalk f g = bisequenceL . bimap f g
+-- @
+class (Bifunctor t, Bifoldable t) => Bicrosswalk t where
+    bicrosswalk :: (Align f) => (a -> f c) -> (b -> f d) -> t a b -> f (t c d)
+    bicrosswalk f g = bisequenceL . bimap f g
+
+    bisequenceL :: (Align f) => t (f a) (f b) -> f (t a b)
+    bisequenceL = bicrosswalk id id
+
+instance Bicrosswalk Either where
+    bicrosswalk f _ (Left x)  = Left  <$> f x
+    bicrosswalk _ g (Right x) = Right <$> g x
+
+instance Bicrosswalk These where
+    bicrosswalk f _ (This x) = This <$> f x
+    bicrosswalk _ g (That x) = That <$> g x
+    bicrosswalk f g (These x y) = align (f x) (g y)
+    -- bisequenceL = these (fmap This) (fmap That) align
