b360e80c8b07ea6b2b894891bce97ccb7d7f4054
Lars Kuhtz <lars@kadena.io>
2021-10-23 10:33:52 -0700

    build with ghc-9.2

--- semialign/src/Data/Semialign/Internal.hs
+++ semialign/src/Data/Semialign/Internal.hs
@@ -28,7 +28,10 @@ import Data.List.NonEmpty                (NonEmpty (..))
 import Data.Maybe                        (catMaybes)
 import Data.Monoid                       (Monoid (..))
 import Data.Proxy                        (Proxy (..))
-import Data.Semigroup                    (Option (..), Semigroup (..))
+#if !MIN_VERSION_base(4,16,0)
+import Data.Semigroup                    (Option (..))
+#endif
+import Data.Semigroup                    (Semigroup (..))
 import Data.Sequence                     (Seq)
 import Data.Tagged                       (Tagged (..))
 import Data.Vector.Fusion.Stream.Monadic (Step (..), Stream (..))
@@ -481,12 +484,14 @@ instance SemialignWithIndex Int NonEmpty
 instance ZipWithIndex Int NonEmpty
 instance RepeatWithIndex Int NonEmpty
 
+#if !MIN_VERSION_base(4,16,0)
 deriving instance Semialign Option
 deriving instance Align Option
 deriving instance Unalign Option
 deriving instance Zip Option
 deriving instance Repeat Option
 deriving instance Unzip Option
+#endif
 
 {-
 deriving instance SemialignWithIndex () Option
@@ -742,7 +747,9 @@ instance Monad m => Semialign (Stream m) where
                 Done -> case (av, adone) of
                     (Just x, False) -> Yield (f $ This x) (sa, sb, Nothing, adone)
                     (_, True)       -> Done
+#if !MIN_VERSION_base(4,16,0)
                     _               -> Skip (sa, sb, Nothing, False)
+#endif
 
 instance Monad m => Zip (Stream m) where
     zipWith = Stream.zipWith
